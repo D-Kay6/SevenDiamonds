3224b946fd7c123c9f9670c93b95edc067207179
Damian Kraaijeveld <damian.kraaijeveld96@gmail.com>
2020-08-13 16:36:23 +0200

    Added first implementation of default layout.

---
+++ SevenDiamonds/Scripts/Typescript/main.ts
@@ -0,0 +1,9 @@
+﻿function openNav() {
+    var sideNav = document.getElementById("sideNavigation");
+    if (sideNav) sideNav.style.width = "250px";
+}
+
+function closeNav() {
+    var sideNav = document.getElementById("sideNavigation");
+    if (sideNav) sideNav.style.width = "0px";
+}
